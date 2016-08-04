5c95334426c47fdeae9366fa6fa8f2c961d6e5f5
思妙林 <linsm8968@126.com>
2016-08-04 11:17:54 +0800

    1.EtcdConfig-V1 2.Test on EtcdConfig-V1

---
+++ src/main/java/com/coreos/jetcd/resolver/EtcdConfig.java
@@ -0,0 +1,7 @@
+package com.coreos.jetcd.resolver;
+
+/**
+ * Created by Sophie on 16/8/4.
+ */
+public class EtcdConfig {
+}
---
+++ src/test/java/com/coreos/jetcd/resolver/EtcdConfigTest.java
@@ -0,0 +1,31 @@
+package com.coreos.jetcd.resolver;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Sophie on 16/8/4.
+ */
+public class EtcdConfigTest {
+    @Test
+    public void testGetEndPoints() throws Exception {
+
+    }
+
+    @Test
+    public void testSetEndPoints() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDialTimeout() throws Exception {
+
+    }
+
+    @Test
+    public void testConfigFromFile() throws Exception {
+
+    }
+
+}
